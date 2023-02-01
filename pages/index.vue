0fac9001b04e4d7b07c1ead0713955a9d41349d8
Charles Joshua Uy <uy.charlesjoshua@yahoo.com>
2023-02-02 02:55:16 +0800

    Initial Config Files
    Added:
    - Nuxt 3
    - TailwindCSS
    - Prisma

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex justify-center items-center h-screen">
+        <h1 class="text-3xl font-bold text-green-500">
+            Installed Tailwind CSS in Nuxt 3
+        </h1>
+    </div>
+</template>
+  
