c7dc4bc3455d24e7d14792283e2c15a3055e06ba
Charles Joshua Uy <uy.charlesjoshua@yahoo.com>
2023-03-15 18:46:33 +0800

    Added and Updated npm packages

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require ("express")
+const app = express()
+const port = 3000
+
+app.listen (port, () => {
+    console.log(
+      `\n(-ω-)ゞ Server is now online!\nListening at http://localhost:${port}\n`
+    );
+} )
