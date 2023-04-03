958c9f88d2eb10f0df3daef99b8be72ee8c028e4
Charles Joshua Uy <cjuy@evc.pshs.edu.ph>
2023-04-03 23:11:52 +0800

    Fixed port numbers

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
-const express = require ("express")
-const app = express()
-const port = 4000
+require("dotenv").config(); //This allows the env file to be read
+const express = require("express");
+const app = express();
+const port = process.env.SERVER_PORT; //The server port number is taken from the .env file
 
 app.listen (port, () => {
     console.log(
       `\n(-ω-)ゞ Server is now online!\nListening at http://localhost:${port}\n`
     );
-} )
+})
