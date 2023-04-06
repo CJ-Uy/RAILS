258c26a101b711e70bae862702b9b6e7b37cc78f
Charles Joshua Uy <cjuy@evc.pshs.edu.ph>
2023-04-06 18:12:21 +0800

    Create Student in database when logging in thru googleoauth

--- server/index.js
+++ server/index.js
@@ -1,18 +1,23 @@
 // --- IMPORTS --- //
 require("dotenv").config(); //This allows the env file to be read
 
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 const express = require("express");
 const session = require("express-session")
 const passport = require("passport");
 const port = process.env.SERVER_PORT; //The server port number is taken from the .env file
 
+
 const app = express(); //Create the express app
+
 app.use(
   session({ secret: "Your Secret Key", resave: true, saveUninitialized: true }) //TODO: Read the express-session documentation to fix deprecated issues
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
+//Importing other scripts
 require("./scripts/auth.js");
 // --- END OF IMPORTS --- //
 
@@ -49,13 +54,21 @@ app.get("/google/callback",
 );
 
 //Handle the success and error situations
-app.get("/dashboard", isLoggedIn, (req, res) => { 
+app.get("/dashboard", isLoggedIn, (req, res) => {
   res.send("<h1>SUCCESS</h1>");
-  const { displayName, emails } = req.user;
-  const email = emails[0].value;
+  const { displayName, family_name, given_name, email, photos } = req.user;
+  const photo = photos[0].value; //TODO: Find what to do if no photo
 
-  // Use the retrieved user details
-  console.log(`Welcome ${displayName}! Your email is ${email}.`); //Idk why i cant res.send these details
+  //Create a new Student in the database
+  create_student(email, given_name, family_name, photo)
+    .then(async () => {
+      await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+      console.error(e);
+      await prisma.$disconnect();
+      process.exit(1);
+    });
 });
 
 app.get("/auth/failure", (req, res) => {
@@ -63,3 +76,19 @@ app.get("/auth/failure", (req, res) => {
   res.send("<a href='/'>Return to HomePage</a>")
 });
 // --- END OF GOOGLE OAUTH LOGIN --- //
+
+
+
+// --- CREATING NEW STUDENT USER --- //
+//TODO: Only allow the evc emails
+async function create_student(email, firstname, lastname, photo) {
+  await prisma.students.create({
+    data: {
+      Email: email,
+      Firstname: firstname,
+      Lastname: lastname,
+      Photo_Link: photo
+    },
+  })
+}
+
