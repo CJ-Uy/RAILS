08c52f9e4cba769a9c0665d21dda7604e039dde1
Charles Joshua Uy <cjuy@evc.pshs.edu.ph>
2023-04-17 09:42:46 +0800

    worked on the form 1 accountability html to pdf

--- server/scripts/pdfs/templates/CID-20_Accountability.js
+++ server/scripts/pdfs/templates/CID-20_Accountability.js
@@ -2,6 +2,36 @@ campus = "EASTERN VISAYAS";
 controlNo = "22-23-0001";
 schoolYear = "2022-2023";
 gradeSection = "11-C";
+numStudents = "30";
+subject = "Physics";
+concurrentTopic = "Light";
+unit = "3";
+teacherInCharge = "Mr. John Doe";
+venue = "Lab 3";
+dates = "Tomorrow";
+timeOfUse = "9:00am";
+
+quantity = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", " ", " "]
+item = ["microscope", "bunsen burner", "petri dish", "test tube", "beaker", "pipette", "test tube rack", "test tube holder", "test tube brush", "test tube clamp", "", ""]
+description = ["to observe", "to heat", "to observe", "to observe", "to observe", "to measure", "to hold", "to hold", "to clean", "to hold", "", ""]
+returned = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", " ", " "]
+issued = [
+	"good",
+	"good",
+	"good",
+	"good",
+	"good",
+	"good",
+	"good",
+	"good",
+	"good",
+	"good",
+	" ",
+	" "
+];
+returned = ["good", "good", "good", "good", "good", "good", "good", "good", "good", "good", " ", " "];
+
+
 
 html = `<!DOCTYPE html>
 <html lang="en">
@@ -39,7 +69,7 @@ html = `<!DOCTYPE html>
         #content {
             position: absolute;
             /*Taken through trial and errror*/
-            width: calc(8.27-(0.955+0.92))in;
+            width: 210mm;
             margin-top: 0.955in;
             margin-left: 0.92in;
         }
@@ -49,36 +79,193 @@ html = `<!DOCTYPE html>
         #heading > h1 {
             margin-bottom: 0.02in; 
         }
+        #title {
+            margin-top: 0.21in;
+            margin-bottom: 0.23in;
+        }
         .input {
-            border-bottom: 2px solid black;
+            border-bottom: 0.5px solid black;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 0.21in;
+        }
+        #basic-info-1{
+            text-align: right;
+            border-bottom: none !important;
+        }
+        .right-side-basic-info {
+            display: inline-block;
+            text-align: right;
+            float: right;
+        }
+        .expander {
+            border-bottom: 0.5px solid black;
+        }
+        .remove-botB {
+            border-bottom: 3px solid white;
+        }
+        #request {
+            text-align: center;
+        }
+        #request td {
+            border: 1px solid black;
+            white-space: pre-wrap; /* allow wrapping of white space */
+            word-wrap: break-word; /* break long words */
+        }
+        .request-header {
+            vertical-align: top;
         }
     </style>
 </head>
 <body>
-    <divn id="content">
+    <div id="content">
         <div id="heading">
             <h1>PHILIPPINE SCIENCE HIGH SCHOOL SYSTEM</h1>
             <h1>CAMPUS: &nbsp;&nbsp; <span class="input">${campus}</span> </h1>
         </div>
 
-        <h2>LABORATORY REQUEST AND EQUIPMENT ACCOUNTABILITY FORM</h2>
+        <h2 id="title">LABORATORY REQUEST AND EQUIPMENT ACCOUNTABILITY FORM</h2>
 
         <table id="basic-info">
-            <tr>
+            <tr id="basic-info-1">
                 <td>
-                Control No: <span class="input">${controlNo}</span> &nbsp;&nbsp;&nbsp;
-                SY: <span class="input">${schoolYear}</span>
+                    Control No: <span class="input">${controlNo}</span> &nbsp;&nbsp;&nbsp;
+                    SY: <span class="input">${schoolYear}</span>
+                </td>
+            </tr>
+            <tr class="expander">
+                <td>
+                    <span class="remove-botB">Grade Level and Section: </span><span class="input">${gradeSection}</span>
+                    <span class="right-side-basic-info"> <span class="remove-botB">&nbsp;Number of Students: </span><span class="input">${numStudents} </span></span>
+                </td>
+            </tr>
+            <tr class="expander">
+                <td>
+                    <span class="remove-botB">Subject: </span><span class="input">${subject}</span>
+                    <span class="right-side-basic-info"><span class="remove-botB">&nbsp;Concurrent Topic: </span><span class="input">${concurrentTopic}</span> </span>
+                </td>
+            </tr>
+            <tr class="expander">
+                <td>
+                    <span class="remove-botB">Unit: </span><span class="input">${unit}</span>
+                    <span class="right-side-basic-info"><span class="remove-botB">&nbsp;Teacher-In-Charge: </span><span class="input">${teacherInCharge}</span> </span>
+                </td>
+            </tr>
+            <tr class="expander">
+                <td>
+                    <span class="remove-botB">Venue of Experiment: </span><span class="input">${venue}</span>
                 </td>
             </tr>
             <tr>
-                <td>
-                    Grade Level and Section: <span class="input">${gradeSection}</span>
+                <td class="expander">
+                    <span class="remove-botB">Date/Inclusive Dates: </span><span class="input">${dates}</span>
+                    <span class="right-side-basic-info"><span class="remove-botB">Inclusive Time of Use: </span><span class="input">${timeOfUse}</span> </span>
                 </td>
+            </tr>
+        </table>
+
+        Materials/Equipment Needed:
+        <table id="request">
+            <tr class="request-header">
+                <td rowspan="2">Quantity</td>
+                <td rowspan="2">Item</td>
+                <td rowspan="2">Description</td>
+                <td>Issued</td>
+                <td>Returned</td>
+            </tr>
+            <tr class="request-header">
+                <td>Condition/Remarks</td>
+                <td>Condition/Remarks</td>
+            </tr>
+            <tr>
+                <td>${quantity[0]}</td>
+                <td>${item[0]}</td>
+                <td>${description[0]}</td>
+                <td>${issued[0]}</td>
+                <td>${returned[0]}</td>
+            </tr>
+            <tr>
+                <td>${quantity[1]}</td>
+                <td>${item[1]}</td>
+                <td>${description[1]}</td>
+                <td>${issued[1]}</td>
+                <td>${returned[1]}</td>
+            </tr>
+            <tr>
+                <td>${quantity[2]}</td>
+                <td>${item[2]}</td>
+                <td>${description[2]}</td>
+                <td>${issued[2]}</td>
+                <td>${returned[2]}</td>
+            </tr>
+            <tr>
+                <td>${quantity[3]}</td>
+                <td>${item[3]}</td>
+                <td>${description[3]}</td>
+                <td>${issued[3]}</td>
+                <td>${returned[3]}</td>
+            </tr>
+            <tr>
+                <td>${quantity[4]}</td>
+                <td>${item[4]}</td>
+                <td>${description[4]}</td>
+                <td>${issued[4]}</td>
+                <td>${returned[4]}</td>
+            </tr>
+            <tr>
+                <td>${quantity[5]}</td>
+                <td>${item[5]}</td>
+                <td>${description[5]}</td>
+                <td>${issued[5]}</td>
+                <td>${returned[5]}</td>
+            </tr>
+            <tr>
+                <td>${quantity[6]}</td>
+                <td>${item[6]}</td>
+                <td>${description[6]}</td>
+                <td>${issued[6]}</td>
+                <td>${returned[6]}</td>
+            </tr>
+            <tr>
+                <td>${quantity[7]}</td>
+                <td>${item[7]}</td>
+                <td>${description[7]}</td>
+                <td>${issued[7]}</td>
+                <td>${returned[7]}</td>
+            </tr>
+            <tr>
+                <td>${quantity[8]}</td>
+                <td>${item[8]}</td>
+                <td>${description[8]}</td>
+                <td>${issued[8]}</td>
+                <td>${returned[8]}</td>
+            </tr>
+            <tr>
+                <td>${quantity[9]}</td>
+                <td>${item[9]}</td>
+                <td>${description[9]}</td>
+                <td>${issued[9]}</td>
+                <td>${returned[9]}</td>
+            </tr>
+            <tr>
+                <td>${quantity[10]}</td>
+                <td>${item[10]}</td>
+                <td>${description[10]}</td>
+                <td>${issued[10]}</td>
+                <td>${returned[10]}</td>
+            </tr>
+            <tr>
+                <td>${quantity[11]}</td>
+                <td>${item[11]}</td>
+                <td>${description[11]}</td>
+                <td>${issued[11]}</td>
+                <td>${returned[11]}</td>
             </tr>
         </table>
     </div>
 
-    
 </body>
 </html>
 `;
