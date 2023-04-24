6001ab4e8c12a7610805b2e97698f8346f819f55
Charles Joshua Uy <cjuy@evc.pshs.edu.ph>
2023-04-24 20:14:13 +0800

    initial commit (cleanup #`)

---
+++ server/api/auth/[...].ts
@@ -0,0 +1,12 @@
+import { NuxtAuthHandler } from "#auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NuxtAuthHandler({
+    providers: [
+        //@ts-ignore
+        GoogleProvider.default({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+    ]
+});
