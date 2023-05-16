e575bab57221965726d624a57aa4102894f63194
Charles Joshua Uy <cjuy@evc.pshs.edu.ph>
2023-05-16 18:10:03 +0800

    added missing code for forms

---
+++ server/app/forms/PDFconverter.js
@@ -0,0 +1,9 @@
+import puppeteer from "puppeteer";
+
+export default async function convertHtmlToPdf(htmlContent,outputPath) {
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
+    await page.setContent(htmlContent);
+    await page.pdf({ path: outputPath, format: "A4" });
+    await browser.close();
+}
