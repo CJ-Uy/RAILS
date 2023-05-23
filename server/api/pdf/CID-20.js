21ad2c4adeea425733e11ea430ab1a8f4ded1381
Charles Joshua Uy <cjuy@evc.pshs.edu.ph>
2023-05-23 22:17:54 +0800

    Modified auth options

--- server/api/pdf/CID-20.js
+++ server/api/pdf/CID-20.js
@@ -1,22 +1,25 @@
 import convertHtmlToPdf from "../../app/forms/PDFconverter.js";
 
-const html = `<html>
-  <head>
-    <title>HTML to PDF Conversion</title>
-    <style>
-      body {
-        font-family: Arial, sans-serif;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>Example HTML to PDF Conversion</h1>
-    <p>This is the content of the PDF.</p>
-  </body>
-</html>`;
+export default defineEventHandler(async () => {
+    const html = `
+      <html>
+        <head>
+          <title>HTML to PDF Conversion</title>
+          <style>
+            body {
+              font-family: Arial, sans-serif;
+            }
+          </style>
+        </head>
+        <body>
+          <h1>Example HTML to PDF Conversion</h1>
+          <p>This is the content of the PDF.</p>
+        </body>
+      </html>`;
 
-const outputPath = "./server/filledForms/examples.pdf";
+    const outputPath = "./server/filledForms/examples.pdf";
 
-convertHtmlToPdf(html, outputPath)
-    .then(() => console.log("PDF generated successfully"))
-    .catch((error) => console.error("Error generating PDF:", error));
+    convertHtmlToPdf(html, outputPath)
+        .then(() => console.log("PDF generated successfully"))
+        .catch((error) => console.error("Error generating PDF:", error));
+});
