d492ebe2c81167b2d18f3eec469e84edf8312bef
Charles Joshua Uy <cjuy@evc.pshs.edu.ph>
2023-07-08 23:36:37 +0800

    fixed route links for landing page

--- components/Landing/Navbar.vue
+++ components/Landing/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <!-- Navigation Bar -->
-        <nav class="relative container min-w-full p-6 bg-primaryDark ml-0">
+        <nav class="relative container min-w-full p-6 bg-primary-100 ml-0">
             <!-- Flex box container -->
             <div class="flex items-center justify-between">
                 <!-- Logo -->
@@ -12,29 +12,31 @@
                 </div>
                 <!-- Menu items -->
                 <div class="hidden md:flex space-x-6 text-offWhite">
-                    <NuxtLink to="/" class="hover:text-secondary cursor-pointer"
+                    <NuxtLink 
+                        to="/" 
+                        class="hover:text-secondary-100 cursor-pointer" 
                         >HOME</NuxtLink
                     >
                     <NuxtLink
                         to="/about"
-                        class="hover:text-secondary cursor-pointer"
+                        class="hover:text-secondary-100 cursor-pointer"
                         >ABOUT</NuxtLink
                     >
                     <NuxtLink
                         to="/services"
-                        class="hover:text-secondary cursor-pointer"
+                        class="hover:text-secondary-100 cursor-pointer"
                         >SERVICES</NuxtLink
                     >
                     <NuxtLink
                         to="/contact"
-                        class="hover:text-secondary cursor-pointer"
+                        class="hover:text-secondary-100 cursor-pointer"
                         >CONTACT US</NuxtLink
                     >
                 </div>
                 <!-- Sign in Button -->
                 <NuxtLink to="/login" class="p-3 px-6 pt-2">
                     <button
-                        class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+                        class="rounded bg-secondary-100 px-4 py-2 font-bold text-white hover:bg-blue-600"
                     >
                         Sign In
                     </button>
@@ -49,5 +51,12 @@
     height: 45px;
     width: 46px;
     margin: 0;
+}
+
+.router-link-active {
+    color: #3AA671;
+}
+.router-link-active:hover {
+    color: #3AA671;
 }
 </style>
