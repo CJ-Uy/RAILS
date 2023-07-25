ec355aa432cef67b723dfb6af29d68f195220c91
Charles Joshua Uy <cjuy@evc.pshs.edu.ph>
2023-07-26 01:52:20 +0800

    fixed issue with revoked emails

--- server/api/user/me.ts
+++ server/api/user/me.ts
@@ -10,23 +10,49 @@ export default eventHandler(async (event) => {
 
     // Database logic
     /*
-        TODO: Check if the email already exists but is under a different name if so change the email of the previous account and store the new one
-        TODO: Check for the ROLE of the user here or on auth metaheads (add ROLE to the return of session)
         TODO: Add the secret button to login page to allow admins to login through username and password
     */
-    const user = await prisma.users.upsert({
+    const userInDatabase = await prisma.users.findFirst({
         where: {
             email: session.user.email,
         },
-        update: {},
-        create: {
-            email: session.user?.email,
-            imageLink: session.user?.image,
-            firstName: session.given_name,
-            lastName: session.family_name,
-            studentProfile: { create: {} },
-        },
     });
-    // console.log(`${JSON.stringify(user)}`); printing user only shows [Object object]
-    return user;
+
+    if (userInDatabase) {
+        // User already exists in the database
+        if (
+            session.given_name !== userInDatabase.firstName ||
+            session.family_name !== userInDatabase.lastName
+        ) {
+            // Old user with the same email gets their email set to revoked
+            await prisma.users.update({
+                where: { id: userInDatabase.id },
+                data: { email: "REVOKED EMAIL" },
+            });
+
+            // Create a new user
+            return await prisma.users.create({
+                data: {
+                    email: session.user?.email,
+                    imageLink: session.user?.image,
+                    firstName: session.given_name,
+                    lastName: session.family_name,
+                    studentProfile: { create: {} },
+                },
+            });
+        }
+    } else {
+        // Create a new user
+        return await prisma.users.create({
+            data: {
+                email: session.user?.email,
+                imageLink: session.user?.image,
+                firstName: session.given_name,
+                lastName: session.family_name,
+                studentProfile: { create: {} },
+            },
+        });
+    }
+
+    return userInDatabase;
 });
