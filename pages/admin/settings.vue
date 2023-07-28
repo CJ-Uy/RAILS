23e620c54b8f65bb7db5c2e9cb2fa1c3ccfbaf54
Charles Joshua Uy <cjuy@evc.pshs.edu.ph>
2023-07-28 16:27:49 +0800

    trying signature paf

---
+++ pages/admin/settings.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <canvas ref="canvas"></canvas>
+        <button @click="clear">Clear</button>
+        <button @click="save">Save</button>
+    </div>
+</template>
+
+<script setup>
+import SignaturePad from "signature_pad";
+
+const signaturePad = new SignaturePad($refs.canvas);
+
+function clear() {
+    signaturePad.clear();
+}
+
+function save() {
+    const signature = signaturePad.toDataURL();
+    console.log(signature);
+    // Save signature to database or do something else with it
+}
+</script>
