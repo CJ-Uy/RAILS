01dd8524dcfa788f3f8777cd9502ea3ba577b6ea
Charles Joshua Uy <cjuy@evc.pshs.edu.ph>
2023-07-30 18:51:24 +0800

    Fix database conflicts

--- prisma/migrations/20230730102912_init/migration.sql
+++ prisma/migrations/20230730102912_init/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE `Users` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
     `firstName` VARCHAR(191) NOT NULL,
     `lastName` VARCHAR(191) NOT NULL,
@@ -8,28 +8,27 @@ CREATE TABLE `Users` (
     `imageLink` VARCHAR(191) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
-    `studentsId` INTEGER NULL,
-    `teachersId` INTEGER NULL,
-    `adminsId` INTEGER NULL,
+    `studentsId` VARCHAR(191) NULL,
+    `teachersId` VARCHAR(191) NULL,
+    `adminsId` VARCHAR(191) NULL,
 
-    UNIQUE INDEX `Users_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Admins` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
-    `signature` BLOB NULL,
+    `signature` VARCHAR(191) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Teachers` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `signature` BLOB NULL,
+    `id` VARCHAR(191) NOT NULL,
+    `signature` VARCHAR(191) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -38,8 +37,8 @@ CREATE TABLE `Teachers` (
 
 -- CreateTable
 CREATE TABLE `Students` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `gradeSectionCode` INTEGER NULL,
+    `id` VARCHAR(191) NOT NULL,
+    `gradeSectionId` VARCHAR(191) NULL,
     `grade` INTEGER NULL,
     `section` VARCHAR(191) NULL,
 
@@ -48,7 +47,7 @@ CREATE TABLE `Students` (
 
 -- CreateTable
 CREATE TABLE `GradeSection` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(191) NOT NULL,
     `grade` INTEGER NOT NULL,
     `section` VARCHAR(191) NOT NULL,
 
@@ -57,49 +56,52 @@ CREATE TABLE `GradeSection` (
 
 -- CreateTable
 CREATE TABLE `Units` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
+    `frequencyRank` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
+    UNIQUE INDEX `Units_frequencyRank_key`(`frequencyRank`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Laboratories` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NULL,
-    `locationId` INTEGER NOT NULL,
+    `locationId` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `LaboratoryLocations` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NULL,
-    `dateAdded` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `dateRemoved` DATETIME(3) NULL,
-    `inventoryOfMaterialsSupplyId` INTEGER NULL,
-    `equipmentId` INTEGER NULL,
-    `reagentId` INTEGER NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `removedAt` DATETIME(3) NULL,
+    `inventoryOfMaterialsSupplyId` VARCHAR(191) NULL,
+    `equipmentId` VARCHAR(191) NULL,
+    `reagentId` VARCHAR(191) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `LaboratoryReservations` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(191) NOT NULL,
+    `laboratoryId` VARCHAR(191) NOT NULL,
     `startTime` DATETIME(3) NOT NULL,
     `endTime` DATETIME(3) NOT NULL,
-    `laboratoriesId` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `InventoryOfMaterials` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(191) NOT NULL,
     `itemName` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
     `additionalPurchase` INTEGER NULL,
@@ -114,7 +116,7 @@ CREATE TABLE `InventoryOfMaterials` (
     `damage` INTEGER NOT NULL DEFAULT 0,
     `balancePerCard` INTEGER NULL,
     `balancePerCount` INTEGER NULL,
-    `laboratoriesId` INTEGER NULL,
+    `laboratoriesId` VARCHAR(191) NULL,
     `requestId` INTEGER NULL,
 
     PRIMARY KEY (`id`)
@@ -122,7 +124,7 @@ CREATE TABLE `InventoryOfMaterials` (
 
 -- CreateTable
 CREATE TABLE `InventoryOfEqupiment` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(191) NOT NULL,
     `equipmentCode` VARCHAR(191) NOT NULL,
     `equipmentName` VARCHAR(191) NOT NULL,
     `serialNumber` VARCHAR(191) NULL,
@@ -136,7 +138,7 @@ CREATE TABLE `InventoryOfEqupiment` (
     `acquisitionType` VARCHAR(191) NULL,
     `supplier` VARCHAR(191) NULL,
     `status` VARCHAR(191) NULL,
-    `laboratoriesId` INTEGER NULL,
+    `laboratoriesId` VARCHAR(191) NULL,
     `requestId` INTEGER NULL,
 
     PRIMARY KEY (`id`)
@@ -144,7 +146,7 @@ CREATE TABLE `InventoryOfEqupiment` (
 
 -- CreateTable
 CREATE TABLE `InventoryOfReagents` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(191) NOT NULL,
     `itemNumber` VARCHAR(191) NULL,
     `casNumber` VARCHAR(191) NULL,
     `chemicalName` VARCHAR(191) NOT NULL,
@@ -162,7 +164,7 @@ CREATE TABLE `InventoryOfReagents` (
     `expiryDate` DATETIME(3) NULL,
     `dateOpened` DATETIME(3) NULL,
     `status` VARCHAR(191) NULL,
-    `laboratoriesId` INTEGER NULL,
+    `laboratoriesId` VARCHAR(191) NULL,
     `requestId` INTEGER NULL,
 
     PRIMARY KEY (`id`)
@@ -171,19 +173,19 @@ CREATE TABLE `InventoryOfReagents` (
 -- CreateTable
 CREATE TABLE `LaboratoryRequests` (
     `id` INTEGER NOT NULL,
-    `requestorId` INTEGER NOT NULL,
+    `requestorId` VARCHAR(191) NOT NULL,
     `controlNumber` VARCHAR(191) NULL,
     `schoolYear` VARCHAR(191) NULL,
     `forSubject` VARCHAR(191) NULL,
     `concurrentTopic` VARCHAR(191) NULL,
-    `unitId` INTEGER NOT NULL,
-    `teacherInChargeId` INTEGER NOT NULL,
-    `Laboratory_Reservation_Id` INTEGER NOT NULL,
+    `unitId` VARCHAR(191) NOT NULL,
+    `teacherInChargeId` VARCHAR(191) NOT NULL,
+    `Laboratory_Reservation_Id` VARCHAR(191) NOT NULL,
     `otherGroupMembers` JSON NULL,
     `isSignedByTeacher` BOOLEAN NOT NULL DEFAULT false,
     `isSignedByAdmin` BOOLEAN NOT NULL DEFAULT false,
-    `signedTeacherId` INTEGER NOT NULL,
-    `signedAdminId` INTEGER NOT NULL,
+    `signedTeacherId` VARCHAR(191) NOT NULL,
+    `signedAdminId` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -198,7 +200,7 @@ ALTER TABLE `Users` ADD CONSTRAINT `Users_teachersId_fkey` FOREIGN KEY (`teacher
 ALTER TABLE `Users` ADD CONSTRAINT `Users_adminsId_fkey` FOREIGN KEY (`adminsId`) REFERENCES `Admins`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Students` ADD CONSTRAINT `Students_gradeSectionCode_fkey` FOREIGN KEY (`gradeSectionCode`) REFERENCES `GradeSection`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `Students` ADD CONSTRAINT `Students_gradeSectionId_fkey` FOREIGN KEY (`gradeSectionId`) REFERENCES `GradeSection`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Laboratories` ADD CONSTRAINT `Laboratories_locationId_fkey` FOREIGN KEY (`locationId`) REFERENCES `LaboratoryLocations`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -213,7 +215,7 @@ ALTER TABLE `LaboratoryLocations` ADD CONSTRAINT `LaboratoryLocations_equipmentI
 ALTER TABLE `LaboratoryLocations` ADD CONSTRAINT `LaboratoryLocations_reagentId_fkey` FOREIGN KEY (`reagentId`) REFERENCES `InventoryOfReagents`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `LaboratoryReservations` ADD CONSTRAINT `LaboratoryReservations_laboratoriesId_fkey` FOREIGN KEY (`laboratoriesId`) REFERENCES `Laboratories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `LaboratoryReservations` ADD CONSTRAINT `LaboratoryReservations_laboratoryId_fkey` FOREIGN KEY (`laboratoryId`) REFERENCES `Laboratories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `InventoryOfMaterials` ADD CONSTRAINT `InventoryOfMaterials_laboratoriesId_fkey` FOREIGN KEY (`laboratoriesId`) REFERENCES `Laboratories`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
