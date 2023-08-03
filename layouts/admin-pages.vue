1ad8f76862341148fce3ab5524334da3474b5688
Charles Joshua Uy <cjuy@evc.pshs.edu.ph>
2023-08-03 20:02:06 +0800

    Mobile responsive

--- layouts/admin-pages.vue
+++ layouts/admin-pages.vue
@@ -9,7 +9,6 @@ try {
     await navigateTo("/login");
 }
 
-
 // Redirect if user has a wrong role
 if (user.data.value.role === "STUDENT") {
     await navigateTo("/student");
@@ -17,18 +16,57 @@ if (user.data.value.role === "STUDENT") {
     await navigateTo("/teacher");
 }
 
+// Toggle menu for mobile
+const showMenu = ref(false);
+async function toggleMenu() {
+    showMenu.value = !showMenu.value;
+}
+
 // Make user info available to children pages through provide/inject (so that they dont have to request it again)
 provide("user", user.data.value);
 </script>
 
 <template>
-    <div class="flex flex-row">
-        <div class="flex basis-[15.276146%] flex-col">
-            <!-- Taken from figma 260/1702-->
-            <UserAdminNavBar />
+    <div>
+        <!-- Desktop Layout -->
+        <div class="hidden md:flex md:flex-row">
+            <div class="flex basis-[15.276146%] flex-col md:block">
+                <!-- Taken from figma 260/1702-->
+                <UserAdminNavBar />
+            </div>
+            <div class="basis-[84.723854%]">
+                <slot v-if="user.data.value" />
+            </div>
         </div>
-        <div class="basis-[84.723854%]">
-            <slot v-if="user.data.value" />
+
+        <!-- Mobile Layout -->
+        <div class="md:hidden">
+            <UButton
+                v-if="!showMenu"
+                icon="i-charm-menu-hamburger"
+                @click="toggleMenu"
+                class="fixed bottom-7 right-7 z-10 md:hidden"
+                size="xl"
+            />
+            <UButton
+                v-else
+                icon="i-material-symbols-cancel"
+                @click="toggleMenu"
+                class="fixed bottom-7 right-7 z-10 md:hidden"
+                size="xl"
+            />
+            <div
+                v-if="showMenu"
+                class="z-9 fixed left-0 top-0 h-full w-full bg-black bg-opacity-50"
+                v-auto-animate
+            >
+                <div class="absolute left-0 top-0 h-full w-[50%]">
+                    <UserAdminNavBar />
+                </div>
+            </div>
+            <div>
+                <slot v-if="user.data.value" />
+            </div>
         </div>
     </div>
 </template>
