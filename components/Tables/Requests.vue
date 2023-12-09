b467177be450642fd633d0bc04e05849cefc82f8
Charles Joshua Uy <129761131+CJ-Uy-EVC@users.noreply.github.com>
2023-12-09 22:07:39 +0800

    started component template

---
+++ components/Tables/Requests.vue
@@ -0,0 +1,43 @@
+<script setup>
+const props = defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+    defaultSortKey: {
+        type: String,
+        required: false,
+        default: "name",
+    },
+    startingColumns: {
+        type: Array,
+        required: true,
+    },
+    listOfAllColumns: {
+        type: Array,
+        required: true,
+    },
+    fetchPath: {
+        type: String,
+        required: true,
+    },
+    updatePath: {
+        type: String,
+        required: false,
+        default: "",
+    },
+    editModeIsOpen: {
+        type: Boolean,
+        required: false,
+        default: false,
+    },
+});
+</script>
+
+<template>
+    <div>
+        <h1>Table Requests</h1>
+    </div>
+</template>
+
+<style scoped></style>
--- components/User/Admin/Requests/Tables/AllRequests.vue
+++ components/User/Admin/Requests/Tables/AllRequests.vue
@@ -1,13 +1,9 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
     <div>
-
+        <TablesRequests title="All Request"  />
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
