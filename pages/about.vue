989dc7bb60b747aa5c61ad75fba593b2636ae30b
Charles Joshua Uy <129761131+CJ-Uy-EVC@users.noreply.github.com>
2024-03-24 23:40:19 +0800

    edited public pages

--- pages/about.vue
+++ pages/about.vue
@@ -3,69 +3,161 @@
 useHead({
     title: "PSHS-EVC LABS | About",
 });
-
-// [auth: false] allows the page to be seen by "Authenticated" and "Unauthenticated" users
 definePageMeta({ auth: false, layout: "landing-pages" });
 // PAGE META END
 </script>
 
 <template>
     <div>
-        <div class="flex h-screen items-center justify-center">
-            <div class="ml-14 text-center" style="margin-top: -190px">
-                <p class="max-w-700 text-3xl text-gray-800">
+        <div class="mt-15 flex items-center justify-center">
+            <div class="mb-15 mx-14 flex flex-col items-center justify-center">
+                <h1
+                    class="mb-10 text-center text-6xl font-extrabold text-main-500"
+                >
+                    ABOUT US
+                </h1>
+                <p
+                    class="max-w-700 w-[75%] text-center text-xl text-gray-800 sm:text-2xl md:text-3xl"
+                >
                     The PSHS-EVC provides its scholars with state-of-the-art
-                    facilities and high-caliber equipment <br />
-                    to hone their scientific proficiency and technological
-                    applications with the aim of producing <br />
-                    globally-competent leaders and professionals.
+                    facilities and high-caliber equipment to hone their
+                    scientific proficiency and technological applications with
+                    the aim of producing globally-competent leaders and
+                    professionals.
                 </p>
             </div>
         </div>
-        <div class="flex flex-col items-center">
-            <div
-                class="mb-[50px] flex h-[360px] w-[60%] flex-row justify-start"
+        <div class="flex w-full flex-col items-center justify-center">
+            <h1
+                class="mt-15 mb-10 text-center text-6xl font-extrabold text-main-500"
             >
-                <div
-                    class="h-full w-[300px] bg-[url('/images/staff/sir_nestor.jpg')] bg-cover bg-center"
-                ></div>
-                <div
-                    class="ml-[50px] flex w-[30%] items-center justify-center text-xl text-gray-800"
-                >
-                    Insert Name and Title
-                </div>
-            </div>
-            <div class="mb-[50px] flex h-[360px] w-[60%] flex-row justify-end">
-                <div
-                    class="mr-[50px] flex w-[30%] items-center justify-center text-xl text-gray-800"
-                >
-                    Insert Name and Title
-                </div>
-                <div
-                    class="h-full w-[300px] bg-[url('/images/staff/sir_nestor.jpg')] bg-cover bg-center"
-                ></div>
-            </div>
+                OUR STAFF
+            </h1>
+
+            <!-- For smaller than medium screens -->
             <div
-                class="mb-[50px] flex h-[360px] w-[60%] flex-row justify-start"
+                class="grid w-full grid-cols-1 items-center justify-items-center gap-4 md:hidden"
             >
+                <!-- Sir Nestor -->
+                <NuxtImg
+                    src="/images/staff/sir_nestor.jpg"
+                    class="w-[50%] md:w-[300px]"
+                    format="webp"
+                />
                 <div
-                    class="h-full w-[300px] bg-[url('/images/staff/sir_nestor.jpg')] bg-cover bg-center"
-                ></div>
-                <div
-                    class="ml-[50px] flex w-[30%] items-center justify-center text-xl text-gray-800"
+                    class="mb-5 flex flex-col justify-center text-center text-2xl text-gray-800"
                 >
-                    Insert Name and Title
+                    <h1 class="font-extrabold">
+                        Mr. Nestor Paderogo Carlon Jr.
+                    </h1>
+                    <h2>Science Research Specialist - SRS</h2>
+                </div>
+
+                <!-- Maam Niña -->
+                <NuxtImg
+                    src="/images/staff/maam_petilla.jpg"
+                    class="w-[50%] md:w-[300px]"
+                    format="webp"
+                />
+                <div
+                    class="mb-5 flex flex-col justify-center text-center text-2xl text-gray-800"
+                >
+                    <h1 class="font-extrabold">Mrs. Niña Petilla</h1>
+                    <h2>Science Research Assistant - SRA</h2>
+                </div>
+
+                <!-- Sir Israel -->
+                <NuxtImg
+                    src="/images/staff/sir_israel.jpg"
+                    class="w-[50%] md:w-[300px]"
+                    format="webp"
+                />
+                <div
+                    class="mb-5 flex flex-col justify-center text-center text-2xl text-gray-800"
+                >
+                    <h1 class="font-extrabold">Mr. Israel Decena Malaca</h1>
+                    <h2>Science Research Assistant - SRA</h2>
+                </div>
+
+                <!-- Maam Cyrill -->
+                <NuxtImg
+                    src="/images/staff/maam_cyrill.jpg"
+                    class="w-[50%] md:w-[300px]"
+                    format="webp"
+                />
+                <div
+                    class="mb-5 flex flex-col justify-center text-center text-2xl text-gray-800"
+                >
+                    <h1 class="font-extrabold">Mrs. Cyrill D. Tapales</h1>
+                    <h2>Science Research Assistant - SRA</h2>
                 </div>
             </div>
-            <div class="mb-[50px] flex h-[360px] w-[60%] flex-row justify-end">
-                <div
-                    class="mr-[50px] flex w-[30%] items-center justify-center text-xl text-gray-800"
-                >
-                    Insert Name and Title
+
+            <!-- For medium and larger screens -->
+            <div
+                class="hidden w-[50%] grid-cols-2 items-center justify-items-center gap-4 md:grid"
+            >
+                <!-- Sir Nestor -->
+                <div>
+                    <NuxtImg
+                        src="/images/staff/sir_nestor.jpg"
+                        class="w-[50%] md:w-[300px]"
+                        format="webp"
+                    />
                 </div>
                 <div
-                    class="h-full w-[300px] bg-[url('/images/staff/sir_nestor.jpg')] bg-cover bg-center"
-                ></div>
+                    class="flex flex-col justify-center text-center text-2xl text-gray-800"
+                >
+                    <h1 class="font-extrabold">
+                        Mr. Nestor Paderogo Carlon Jr.
+                    </h1>
+                    <h2>Science Research Specialist - SRS</h2>
+                </div>
+
+                <!-- Maam Niña -->
+                <div
+                    class="flex flex-col justify-center text-center text-2xl text-gray-800"
+                >
+                    <h1 class="font-extrabold">Mrs. Niña Petilla</h1>
+                    <h2>Science Research Assistant - SRA</h2>
+                </div>
+                <div>
+                    <NuxtImg
+                        src="/images/staff/maam_petilla.jpg"
+                        class="w-[50%] md:w-[300px]"
+                        format="webp"
+                    />
+                </div>
+
+                <!-- Sir Israel -->
+                <div>
+                    <NuxtImg
+                        src="/images/staff/sir_israel.jpg"
+                        class="w-[50%] md:w-[300px]"
+                        format="webp"
+                    />
+                </div>
+                <div
+                    class="flex flex-col justify-center text-center text-2xl text-gray-800"
+                >
+                    <h1 class="font-extrabold">Mr. Israel Decena Malaca</h1>
+                    <h2>Science Research Assistant - SRA</h2>
+                </div>
+
+                <!-- Maam Cyrill -->
+                <div
+                    class="flex flex-col justify-center text-center text-2xl text-gray-800"
+                >
+                    <h1 class="font-extrabold">Mrs. Cyrill D. Tapales</h1>
+                    <h2>Science Research Assistant - SRA</h2>
+                </div>
+                <div>
+                    <NuxtImg
+                        src="/images/staff/maam_cyrill.jpg"
+                        class="w-[50%] md:w-[300px]"
+                        format="webp"
+                    />
+                </div>
             </div>
         </div>
     </div>
--- pages/login.vue
+++ pages/login.vue
@@ -16,23 +16,18 @@ const { signIn } = useAuth();
 <template>
     <div class="flex h-screen flex-col md:flex-row">
         <div
-            class="flex basis-[50%] flex-col items-center justify-center bg-light-primary text-center"
+            class="flex basis-[50%] flex-col items-center justify-center text-center text-white"
         >
-            <p class="mx-10 text-3xl text-white">
-                We use the EVC email as proof you are a bonfide student. Hence,
-                if you do not have an EVC email or cannot access it, you will
-                not be able to use this service.
-            </p>
-            <p class="mx-10 mt-5 text-lg text-white">
-                However, as this website is not yet official, it's sign in
-                through EVC is currently blocked by Google. Hence, please use
-                any personal account with a @gmail.com email to sign in.
-            </p>
+            <NuxtImg
+                src="/images/loginArt.png"
+                format="webp"
+                class="h-full md:h-screen"
+            />
         </div>
         <div
             class="md:h[100%] flex basis-[50%] flex-col items-center justify-center bg-white"
         >
-            <UCard class="w-[80%] text-center">
+            <UCard class="mt-10 w-[80%] text-center md:mt-0">
                 <template #header>
                     <h1 class="text-3xl font-extrabold text-main-500">
                         SIGN IN TO RAILS
@@ -46,7 +41,22 @@ const { signIn } = useAuth();
                     padding="xl"
                     @click="signIn('google')"
                 />
+                <template #footer>
+                    <p class="mx-10 text-center text-lg">
+                        We use the EVC email as proof you are a bonfide student.
+                        Hence, if you do not have an EVC email or cannot access
+                        it, you will not be able to use this service.
+                    </p>
+                </template>
             </UCard>
+            <p class="mx-20 mt-5 text-center text-lg">
+                However, as this website is not yet official, it's sign in
+                through EVC is currently blocked by Google. Hence,
+                <span class="text-red-500">
+                    please actually use any personal account with a @gmail.com
+                    email to sign in.
+                </span>
+            </p>
         </div>
     </div>
 </template>
