d203e2147b10fdaf2b520d59c0f880bb34cc39f7
Charles Joshua Uy <129761131+CJ-Uy-EVC@users.noreply.github.com>
2024-03-25 09:24:23 +0800

    Delete works

--- server/api/admin/editForms/manageSY/deleteAllRequests.js
+++ server/api/admin/editForms/manageSY/deleteAllRequests.js
@@ -4,15 +4,15 @@ export default defineEventHandler(async (event) => {
     const newData = await readBody(event);
 
     await prisma.$transaction([
-        // Delete related records in ReagentRequests first
+        prisma.laboratoryReservations.deleteMany({
+            where: { laboratoryRequestId: newData.id },
+        }),
         prisma.reagentRequests.deleteMany({
             where: { laboratoryRequestId: newData.id },
         }),
-        // Then delete records in MaterialRequests that reference laboratoryRequestId
         prisma.materialRequests.deleteMany({
             where: { laboratoryRequestId: newData.id },
         }),
-        // Then delete records in EquipmentRequests, and finally LaboratoryRequests
         prisma.equipmentRequests.deleteMany({
             where: { schoolYearId: newData.id },
         }),
